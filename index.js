5da943d6a808d0133986a7aa84d676169c636c2e
israrkhann08 <israrkhann08@gmail.com>
2024-11-21 23:08:54 +0500

    backend-set

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const http = require('http');
 const express = require('express');
 const port = 9000;
 const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server); // this io handle socket
+
+
+// Socket.io
+io.on('connection', (socket) => {
+    console.log("A new User has connected", socket.id); // it automatically manage   
+})
 
 app.use(express.static(path.resolve('./public')));
 
