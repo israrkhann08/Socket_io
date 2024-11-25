29e8659efd9a795bb501b363bf1f45baf69a44ca
israrkhann08 <israrkhann08@gmail.com>
2024-11-25 21:59:26 +0500

    socket full_implement

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const http = require('http');
-const express = require('express');
-const port = 9000;
-const path = require('path');
-const { Server } = require('socket.io');
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server); // this io handle socket
-
+const io = new Server(server);
 
 // Socket.io
-io.on('connection', (socket) => {
-    console.log("A new User has connected", socket.id); // it automatically manage   
-})
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-app.use(express.static(path.resolve('./public')));
+  // Listen for user messages
+  socket.on("user-message", (message) => {
+    console.log(`Message received: ${message}`);
+    io.emit("message", message); // Broadcast to all clients
+  });
 
-app.get('/', (req, res) => {
-    return res.sendFile("./public/index.html");
-})
+  // Handle user disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
-server.listen(port, () => {
-  console.log(`app is running on port ${port}`);
-})
+// Serve static files
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+server.listen(9000, () => console.log(`Server Started at PORT: 9000`));
